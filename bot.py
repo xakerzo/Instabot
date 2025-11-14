671969e5bdc9f28608744e10d64260e807b4c744
xakerzo <37242980+xakerzo@users.noreply.github.com>
2025-11-14 21:16:28 +0500

    Refactor bot commands and update message responses

--- bot.py
+++ bot.py
@@ -4,19 +4,23 @@ from config import BOT_TOKEN
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher(bot)
 
-@dp.message_handler(content_types=['text'])
-async def change_instagram_domain(message: types.Message):
-    text = message.text.strip()
+@dp.message_handler(commands=['start'])
+async def start(msg: types.Message):
+    await msg.answer("Instagram link yuboring!")
 
-    # faqat instagram linklarini tekshiramiz
+@dp.message_handler()
+async def handle(msg: types.Message):
+    text = msg.text.strip()
+
     if "instagram.com" in text:
-        # domenni kkinstagram.com ga almashtiramiz
+        # domenni almashtiramiz
         new_link = text.replace("www.instagram.com", "kkinstagram.com")
         new_link = new_link.replace("instagram.com", "kkinstagram.com")
 
-        await message.answer(f"Mana o'zgartirilgan link:\n{new_link}")
+        await msg.answer("Mana o‘zgartirilgan link👇")
+        await msg.answer(new_link)
     else:
-        await message.answer("Iltimos, Instagram videosi linkini yuboring!")
+        await msg.answer("Iltimos faqat Instagram link yuboring!")
 
-if __name__ == "__main__":
-    executor.start_polling(dp)
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
