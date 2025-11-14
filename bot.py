7e2d0d42233968a5b84862fb0537431fa4667836
xakerzo <37242980+xakerzo@users.noreply.github.com>
2025-11-14 21:33:00 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,26 +4,33 @@ from config import BOT_TOKEN
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher(bot)
 
+
 @dp.message_handler(commands=['start'])
 async def start(msg: types.Message):
-    await msg.answer("MENGA *INSTAGRAM VIDEO LINKINI* YUBORING, SIZGA VIDEO QILIB YUBORAMAN!
-    "Sizga bular ham kerak bo‘lsa:\n"
-        "📌 TikTok VIDEO yuklab beruvchi bot: @tiktokdan_yuklabot\n"
-        "📌 Pinterest RASM & VIDEO yuklab beruvchi bot: @pinterest_yuklabot")
+    text = """SALOM! 👋
+MENGA *INSTAGRAM VIDEO LINKINI* YUBORING, SIZGA VIDEO QILIB YUBORAMAN!
+
+Sizga bular ham kerak bo‘lsa:
+📌 TikTok VIDEO yuklab beruvchi bot: @tiktokdan_yuklabot
+📌 Pinterest RASM & VIDEO yuklab beruvchi bot: @pinterest_yuklabot
+"""
+    await msg.answer(text, parse_mode="Markdown")
+
 
 @dp.message_handler()
-async def handle(msg: types.Message):
+async def handle_instagram(msg: types.Message):
     text = msg.text.strip()
 
     if "instagram.com" in text:
-        # domenni almashtiramiz
+        # domenni almashtirish
         new_link = text.replace("www.instagram.com", "kkinstagram.com")
         new_link = new_link.replace("instagram.com", "kkinstagram.com")
 
-        await msg.answer("VIDEO TAYYOR YUKLAB OLISHINGIZ MUMKIN!👇")
+        await msg.answer("VIDEO TAYYOR YUKLAB OLISHINGIZ MUMKIN! 👇")
         await msg.answer(new_link)
     else:
-        await msg.answer("Iltimos faqat Instagram link yuboring!")
+        await msg.answer("Iltimos Instagram video link yuboring!")
+        
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True)
