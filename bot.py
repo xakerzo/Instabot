5f175290f847a1d184407ec32fdbc293305a225b
xakerzo <37242980+xakerzo@users.noreply.github.com>
2025-11-14 21:49:56 +0500

    Refactor bot.py to improve message handling
    Updated the start command response and modified the handling of Instagram links.

--- bot.py
+++ bot.py
@@ -4,33 +4,23 @@ from config import BOT_TOKEN
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher(bot)
 
-
 @dp.message_handler(commands=['start'])
 async def start(msg: types.Message):
-    text = """SALOM! 👋
-MENGA *INSTAGRAM VIDEO LINKINI* YUBORING, SIZGA VIDEO QILIB YUBORAMAN!
-
-Sizga bular ham kerak bo‘lsa:
-📌 TikTok VIDEO yuklab beruvchi bot: @tiktokdan_yuklabot
-📌 Pinterest RASM & VIDEO yuklab beruvchi bot: @pinterest_yuklabot
-"""
-    await msg.answer(text, parse_mode="Markdown")
-
+    await msg.answer("MENGA *INSTAGRAM VIDEO LINKINI* YUBORING, SIZGA VIDEO QILIB YUBORAMAN!")
 
 @dp.message_handler()
-async def handle_instagram(msg: types.Message):
+async def handle(msg: types.Message):
     text = msg.text.strip()
 
     if "instagram.com" in text:
-        # domenni almashtirish
+        # domenni almashtiramiz
         new_link = text.replace("www.instagram.com", "kkinstagram.com")
         new_link = new_link.replace("instagram.com", "kkinstagram.com")
 
         await msg.answer("VIDEO TAYYOR YUKLAB OLISHINGIZ MUMKIN! 👇")
         await msg.answer(new_link)
     else:
-        await msg.answer("Iltimos Instagram video link yuboring!")
-        
+        await msg.answer("Iltimos faqat Instagram link yuboring!")
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
